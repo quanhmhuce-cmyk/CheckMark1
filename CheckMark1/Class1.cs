8a1091bd1b7151dc51d18d72cc0d19aa761f9b69
quanhmhuce-cmyk <quanhmhuce@gmail.com>
2025-11-13 16:08:36 +0700

    Add project files.

---
+++ CheckMark1/Class1.cs
@@ -0,0 +1,47 @@
+﻿using Autodesk.Revit.DB;
+using System.IO;
+using System.Threading;
+using Autodesk.Revit.UI;
+namespace WpfExternalApp
+{
+    public class EventWatcher
+    {
+        private readonly UIApplication _uiapp;
+
+        public EventWatcher(UIApplication app)
+        {
+            _uiapp = app;
+        }
+
+        public void Start()
+        {
+            new Thread(() =>
+            {
+                while (true)
+                {
+                    if (File.Exists(@"C:\Temp\trigger.txt"))
+                    {
+                        string cmd = File.ReadAllText(@"C:\Temp\trigger.txt");
+                        if (cmd == "HighlightWalls")
+                        {
+                            HighlightWalls();
+                            File.Delete(@"C:\Temp\trigger.txt");
+                        }
+                    }
+                    Thread.Sleep(2000);
+                }
+            })
+            { IsBackground = true }.Start();
+        }
+
+        private void HighlightWalls()
+        {
+            UIDocument uidoc = _uiapp.ActiveUIDocument;
+            var walls = new FilteredElementCollector(uidoc.Document)
+                .OfCategory(BuiltInCategory.OST_Walls)
+                .WhereElementIsNotElementType();
+
+            TaskDialog.Show("Revit API", $"Đã tìm thấy {walls.GetElementCount()} tường trong mô hình!");
+        }
+    }
+}
---
+++ CheckMark1/Command.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Autodesk.Revit.Attributes;
+using Autodesk.Revit.DB;
+using Autodesk.Revit.UI;
+
+namespace CheckMark1
+{
+    internal class Command
+    {
+    }
+}
